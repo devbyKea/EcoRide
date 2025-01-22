6394a2ccb35de419d1995d11a2cbd8a8482902f0
devbyKea <keanuprofrance@gmail.com>
2025-01-22 19:00:21 +0100

    Création form inscription + Ajout fonction login/register menu deroulant navbar

--- js/connexion.js
+++ js/connexion.js
@@ -1,4 +1,22 @@
 // script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const userLogo = document.getElementById('user-logo');
+  const dropdownMenu = document.getElementById('profile-dropdown-menu');
+
+  userLogo.addEventListener('click', (event) => {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === 'block' ? 'none' : 'block';
+  });
+
+  // Fermer le menu si on clique ailleurs
+  document.addEventListener('click', (event) => {
+    if (!userLogo.contains(event.target) && !dropdownMenu.contains(event.target)) {
+      dropdownMenu.style.display = 'none';
+    }
+  });
+});
+
 console.log("Le script est chargé !");
 
   // Gestion du menu hamburger
