d285420226a49778ca9aee305238dfe1c6bc7c5c
devbyKea <keanuprofrance@gmail.com>
2025-02-02 17:59:27 +0100

    modif config.php

--- php/config.php
+++ php/config.php
@@ -1,13 +1,14 @@
 <?php
-$host = getenv("MYSQLHOST") ?: "mysql.railway.internal";  // Remplace par ton host Railway
+$host = getenv("PMA_HOST") ?: "mysql.railway.internal";  // Remplace par ton host Railway
 $dbname = getenv("MYSQLDATABASE") ?: "railway"; // Mets le nom correct
-$username = getenv("MYSQLUSER") ?: "root"; // Ton utilisateur
-$password = getenv("MYSQLPASSWORD") ?: "hbdBKLksPtwzoEqeupeFGMEoYwvXnnLT"; // Ton mot de passe
-$port = getenv("MYSQLPORT") ?: "3306"; // Vérifie le port
+$username = getenv("PMA_USER") ?: "root"; // Ton utilisateur
+$password = getenv("PMA_PASSWORD") ?: "hbdBKLksPtwzoEqeupeFGMEoYwvXnnLT"; // Ton mot de passe
+$port = getenv("PMA_PORT") ?: "3306"; // Vérifie le port
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à MySQL !";
 } catch (PDOException $e) {
     die("Erreur de connexion à MySQL : " . $e->getMessage());
 }
