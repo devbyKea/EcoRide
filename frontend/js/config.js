e86bc1549f6e89ae8b49ebd61d3d884e342dceaf
devbyKea <keanuprofrance@gmail.com>
2025-02-10 14:03:00 +0100

    Configuration Vercel

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,2 @@
-const API_URL = "https://ecoride-production.up.railway.app";
+export const API_URL = "https://ecoride-production.up.railway.app";
 
-export default API_URL;
-
-console.log("API_URL utilisé :", API_URL);
