1f7e39884ff2f5c2726b56d42aeb02a4e68d2213
devbyKea <keanuprofrance@gmail.com>
2025-02-10 17:50:24 +0100

    Correction CORS sur tous les fichiers API

--- php/api/reset_password.php
+++ php/api/reset_password.php
@@ -1,4 +1,15 @@
 <?php
+// Activer CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Gérer les requêtes OPTIONS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 header("Content-Type: application/json");
 require_once __DIR__ . '/../config/database.php';
 
