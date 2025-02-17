35d5ea64a40a0604d5647f2e3cb8c573166697dc
devbyKea <keanuprofrance@gmail.com>
2025-02-17 21:25:01 +0100

    modif script navabar

--- frontend/js/trajets.js
+++ frontend/js/trajets.js
@@ -1,24 +1,4 @@
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const userLogo = document.getElementById('user-logo');
-  const dropdownMenu = document.getElementById('profile-dropdown-menu');
-
-  userLogo.addEventListener('click', (event) => {
-    event.preventDefault(); // Empêche le comportement par défaut du lien
-    dropdownMenu.style.display =
-      dropdownMenu.style.display === 'block' ? 'none' : 'block';
-  });
-
-  // Fermer le menu si on clique ailleurs
-  document.addEventListener('click', (event) => {
-    if (!userLogo.contains(event.target) && !dropdownMenu.contains(event.target)) {
-      dropdownMenu.style.display = 'none';
-    }
-  });
-});
-
-  // Fonction pour mettre en majuscule la première lettre de chaque mot
+// Fonction pour mettre en majuscule la première lettre de chaque mot
   function capitalize(input) {
     return input
       .toLowerCase()
